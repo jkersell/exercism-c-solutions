5672191e61945e0e25c457023d73c46d524f4125
Jazz Kersell <jazzkersell@gmail.com>
2023-09-11 13:02:37 -0400

    c/bob: Rename variables for better clarity

--- c/bob/bob.c
+++ c/bob/bob.c
@@ -10,15 +10,15 @@ char *hey_bob(char *greeting) {
         return "Fine. Be that way!";
 
     bool whitespace_only = true;
-    bool upper_case = false;
-    bool lower_case = false;
+    bool contains_upper_case = false;
+    bool contains_lower_case = false;
     bool question = false;
 
     for (size_t i = 0; greeting[i] != '\0'; i++) {
         char c = greeting[i];
         whitespace_only = whitespace_only && isspace(c);
-        upper_case = upper_case || isupper(c);
-        lower_case = lower_case || islower(c);
+        contains_upper_case = contains_upper_case || isupper(c);
+        contains_lower_case = contains_lower_case || islower(c);
         // Once we've seen a question mark, the string should only be considered
         // a question if the only remaining characters are whitespace.
         if (question && !isspace(c))
@@ -31,7 +31,7 @@ char *hey_bob(char *greeting) {
     if (whitespace_only)
         return "Fine. Be that way!";
 
-    bool yelling = upper_case && !lower_case;
+    bool yelling = contains_upper_case && !contains_lower_case;
     if (question && !yelling)
         return "Sure.";
 
