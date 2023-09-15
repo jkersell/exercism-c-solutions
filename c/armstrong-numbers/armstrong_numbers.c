15bf1ba47280870e5168f8753ffd679e96d7b58c
Jazz Kersell <jazzkersell@gmail.com>
2023-09-14 23:21:17 -0400

    c/armstrong-numbers: Implement the full calculation

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,5 +1,23 @@
 #include "armstrong_numbers.h"
 
+#include <math.h>
+
+static int count_digits(int num) {
+    int count = 0;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
 bool is_armstrong_number(int candidate) {
-    return candidate / 10 == 0;
+    int digit_count = count_digits(candidate);
+    int total = 0;
+    int reduced = candidate;
+    while (reduced != 0) {
+        total += pow(reduced % 10, digit_count);
+        reduced /= 10;
+    }
+    return total == candidate;
 }
