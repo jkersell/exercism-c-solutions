ac9d496cf0529710c386be21c4d80d2934c819ae
Jazz Kersell <jazzkersell@gmail.com>
2023-09-22 18:48:29 -0400

    bash/darts: Solve the main part of the exercise
    This exercise is pretty straight forward so I just solved it in one shot
    without really doing TDD or using git tactically.

--- bash/darts/darts.sh
+++ bash/darts/darts.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main() {
+    hit_distance=$(echo "sqrt($1 * $1 + $2 * $2)" | bc -l)
+    if (( $(echo "$hit_distance > 10" | bc -l) )); then
+        echo "0"
+    elif (( $(echo "$hit_distance > 5 && $hit_distance <= 10" | bc -l) )); then
+        echo "1"
+    elif (( $(echo "$hit_distance > 1 && $hit_distance <= 5" | bc -l) )); then
+        echo "5"
+    else
+        echo "10"
+    fi
+}
+
+main "$@"
