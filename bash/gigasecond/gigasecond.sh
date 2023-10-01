0577007a86a218f6c967f7210eed8fa9885d9ec6
Jazz Kersell <jazzkersell@gmail.com>
2023-10-01 11:04:28 -0400

    bash/gigasecond: Use long options for better clarity

--- bash/gigasecond/gigasecond.sh
+++ bash/gigasecond/gigasecond.sh
@@ -3,7 +3,7 @@
 GIGASECOND=1000000000
 
 main () {
-    TZ=UTC printf '%(%Y-%m-%dT%H:%M:%S)T\n' "$(( "$(date -u -d "$1" +%s)" + $GIGASECOND ))"
+    TZ=UTC printf '%(%Y-%m-%dT%H:%M:%S)T\n' "$(( "$(date --utc --date="$1" +%s)" + $GIGASECOND ))"
 }
 
 main "$@"
