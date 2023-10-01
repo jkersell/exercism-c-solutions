fc74bfe8d3ad423c66e8f1ee10f94dbba981cd46
Jazz Kersell <jazzkersell@gmail.com>
2023-10-01 11:05:40 -0400

    bash/gigasecond: Export the TZ variable
    Exporting the TZ variable cleans up the solution a bit by removing the
    need for specifying the --utc option to date, as well as allowing the
    printf to have its own line.

--- bash/gigasecond/gigasecond.sh
+++ bash/gigasecond/gigasecond.sh
@@ -3,7 +3,8 @@
 GIGASECOND=1000000000
 
 main () {
-    TZ=UTC printf '%(%Y-%m-%dT%H:%M:%S)T\n' "$(( "$(date --utc --date="$1" +%s)" + $GIGASECOND ))"
+    export TZ=UTC
+    printf '%(%Y-%m-%dT%H:%M:%S)T\n' "$(( "$(date --date="$1" +%s)" + $GIGASECOND ))"
 }
 
 main "$@"
