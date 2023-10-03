651bd154a68b1432735c3fcd7e36348bae83beae
Jazz Kersell <jazzkersell@gmail.com>
2023-10-02 23:23:35 -0400

    go/linked-list: Rework Push/Pop implementations for clarity
    The new implementations more clearly group handling of special cases
    like an empty list or a single element list.

--- go/linked-list/linked_list.go
+++ go/linked-list/linked_list.go
@@ -33,39 +33,47 @@ func (n *Node) Prev() *Node {
 
 func (l *List) Unshift(v interface{}) {
 	newNode := &Node{Value: v}
-	if l.head != nil {
-		newNode.next = l.head
-		l.head.prev = newNode
-	}
-	l.head = newNode
-	if l.tail == nil {
+
+	if l.head == nil {
+		l.head = newNode
 		l.tail = newNode
+		return
 	}
+
+	newNode.next = l.head
+	l.head.prev = newNode
+	l.head = newNode
 }
 
 func (l *List) Push(v interface{}) {
 	newNode := &Node{Value: v}
-	if l.tail != nil {
-		newNode.prev = l.tail
-		l.tail.next = newNode
-	}
-	l.tail = newNode
-	if l.head == nil {
+
+	if l.tail == nil {
 		l.head = newNode
+		l.tail = newNode
+		return
 	}
+
+	newNode.prev = l.tail
+	l.tail.next = newNode
+	l.tail = newNode
 }
 
 func (l *List) Shift() (interface{}, error) {
 	if l.head == nil {
 		return nil, errors.New("Cannot shift an empty list")
 	}
+
+	if l.head == l.tail {
+		result := l.head.Value
+		l.head = nil
+		l.tail = nil
+		return result, nil
+	}
+
 	result := l.head.Value
 	l.head = l.head.next
-	if l.head != nil {
-		l.head.prev = nil
-	} else {
-		l.tail = l.head
-	}
+	l.head.prev = nil
 	return result, nil
 }
 
@@ -73,13 +81,17 @@ func (l *List) Pop() (interface{}, error) {
 	if l.tail == nil {
 		return nil, errors.New("Cannot pop from an empty list")
 	}
+
+	if l.head == l.tail {
+		result := l.tail.Value
+		l.head = nil
+		l.tail = nil
+		return result, nil
+	}
+
 	result := l.tail.Value
 	l.tail = l.tail.prev
-	if l.tail != nil {
-		l.tail.next = nil
-	} else {
-		l.head = l.tail
-	}
+	l.tail.next = nil
 	return result, nil
 }
 
