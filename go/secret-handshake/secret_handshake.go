7c17407faa4c4dfc76fcd680fb2d8e92c3eafe9f
Jazz Kersell <jazzkersell@gmail.com>
2023-10-09 11:42:13 -0400

    go/secret-handshake: Don't reverse order in a separate loop
    The order can be set correctly with a single loop through the code by
    changing the direction we iterate through each bit of the code.

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -9,21 +9,18 @@ var actions = []string{
 
 func Handshake(code uint) []string {
 	var result []string
-	for i := 0; i < 4; i++ {
+
+	start, stop, increment := 0, 4, 1
+	if (1<<4)&code != 0 {
+		start, stop, increment = 3, -1, -1
+	}
+
+	for i := start; i != stop; i += increment {
 		if (1<<i)&code == 0 {
 			continue
 		}
 		result = append(result, actions[i])
 	}
 
-	if (1<<4)&code != 0 {
-		reverseSliceStr(result)
-	}
 	return result
-}
-
-func reverseSliceStr(s []string) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
 }
