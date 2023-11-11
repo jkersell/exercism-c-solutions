e84251d78af39a2f05659a0c44b96a0c67f14be6
Jazz Kersell <jazzkersell@gmail.com>
2023-11-10 22:48:42 -0500

    clojure/leap: Complete the exercise
    This solution uses the observation that if a year is evenly divisible by
    400 then it is also divisible by 100 and 4 so those tests do not need to
    run. Similarily, if a year of divisible by 100, it is also divisible by
    4 and that test does not need to be run.

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,5 +1,9 @@
 (ns leap)
 
-(defn leap-year? [year] ;; <- argslist goes here
-  ;; your code goes here
+(defn leap-year? [year]
+  (cond
+    (= 0 (rem year 400)) true
+    (= 0 (rem year 100)) false
+    (= 0 (rem year 4)) true
+    :else false)
 )
