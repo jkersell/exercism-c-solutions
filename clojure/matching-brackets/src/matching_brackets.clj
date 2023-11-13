5689a197a35822f3d11ee3005b73125fc6b92299
Jazz Kersell <jazzkersell@gmail.com>
2023-11-12 22:01:35 -0500

    clojure/matching-brackets: Handle the remaining types of characters
    Non-bracket characters can just be ignored. The solution needs to handle
    round brackets, so this commit handles them.

--- clojure/matching-brackets/src/matching_brackets.clj
+++ clojure/matching-brackets/src/matching_brackets.clj
@@ -6,8 +6,10 @@
       (reduce
         (fn [acc c]
           (case c
-            (\[ \{) (conj acc c)
+            (\[ \{ \() (conj acc c)
             \]      (if (= \[ (peek acc)) (pop acc) (reduced (conj acc c)))
-            \}      (if (= \{ (peek acc)) (pop acc) (reduced (conj acc c)))))
+            \}      (if (= \{ (peek acc)) (pop acc) (reduced (conj acc c)))
+            \)      (if (= \( (peek acc)) (pop acc) (reduced (conj acc c)))
+            acc))
         openings
         (seq text)))))
