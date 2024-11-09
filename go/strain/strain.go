6d70d44a9f5cf9297ae1a6bfae7bd966e09b33a7
Jazz Kersell <jazzkersell@gmail.com>
2024-11-09 11:39:02 -0500

    go/strain: Implement a solution

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,7 +1,21 @@
 package strain
 
-// Implement the "Keep" and "Discard" function in this file.
+func Keep[T any](items []T, filter func(T) bool) []T {
+	var result []T
+	for _, entry := range items {
+		if filter(entry) {
+			result = append(result, entry)
+		}
+	}
+	return result
+}
 
-// You will need typed parameters (aka "Generics") to solve this exercise.
-// They are not part of the Exercism syllabus yet but you can learn about
-// them here: https://go.dev/tour/generics/1
+func Discard[T any](items []T, filter func(T) bool) []T {
+	var result []T
+	for _, entry := range items {
+		if !filter(entry) {
+			result = append(result, entry)
+		}
+	}
+	return result
+}
