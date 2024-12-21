b61cfbefb33f69bfb81851a42307b8c12aec1edb
Jazz Kersell <jazzkersell@gmail.com>
2024-12-20 19:57:30 -0500

    sqlite/difference-of-squares: Add my initial solution

--- sqlite/difference-of-squares/difference-of-squares.sql
+++ sqlite/difference-of-squares/difference-of-squares.sql
@@ -1,2 +1,12 @@
 -- Schema: CREATE TABLE "difference-of-squares" ("number" INT", property" TEXT, "result" INT);
 -- Task: update the difference-of-squares table and set the result based on the number and property fields.
+
+UPDATE "difference-of-squares" SET result = CASE
+    WHEN property = 'squareOfSum'         THEN
+        POW((number * (number + 1)) / 2, 2)
+    WHEN property = 'sumOfSquares'        THEN
+        (number * (number + 1) * (2 * number + 1)) / 6
+    WHEN property = 'differenceOfSquares' THEN
+        POW((number * (number + 1)) / 2, 2) -
+        (number * (number + 1) * (2 * number + 1)) / 6
+END
